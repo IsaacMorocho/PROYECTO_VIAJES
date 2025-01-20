5327f36a0220d83ac13df1f2e25f19f8806f41f2
IsaacMorocho <IsaacMorocho>
2025-01-20 15:56:33 -0500

    Initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,7 @@
+package org.example;
+public class Main {
+    public static void main(String[] args) {
+    String url="mongodb+srv://joshuamorocho:Joshua2002@cluster0.rc4vm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+    }
+}

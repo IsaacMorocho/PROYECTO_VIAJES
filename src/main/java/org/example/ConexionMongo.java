a6cd59ada3c24ad5fbe1b760dd4c9a1d4bc19975
IsaacMorocho <IsaacMorocho>
2025-01-21 20:52:35 -0500

    Prueba conexion a atlas y muestra de login

---
+++ src/main/java/org/example/ConexionMongo.java
@@ -0,0 +1,20 @@
+package org.example;
+import com.mongodb.MongoClientSettings;
+import com.mongodb.ServerAddress;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoDatabase;
+import java.util.Collections;
+public class ConexionMongo {
+    private static final String HOST = "localhost";
+    private static final int PORT = 27017;
+    private static final String DATABASE = "TravelBuddy";
+
+    public static MongoDatabase getDatabase() {
+        MongoClientSettings settings = MongoClientSettings.builder()
+                .applyToClusterSettings(builder -> builder.hosts(Collections.singletonList(new ServerAddress(HOST, PORT))))
+                .build();
+        MongoClient mongoClient = MongoClients.create(settings);
+        return mongoClient.getDatabase(DATABASE);
+    }
+}
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,18 @@
 package org.example;
+
+import javax.swing.*;
+import java.awt.*;
 public class Main {
     public static void main(String[] args) {
-    String url="mongodb+srv://joshuamorocho:Joshua2002@cluster0.rc4vm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+        String url = "mongodb+srv://joshuamorocho:Joshua2002@cluster0.rc4vm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+        ConexionMongo.getDatabase();
+        JFrame frame = new JFrame("TRAVELBUDDY");
+        frame.setContentPane(new LOGIN().loginP);
+        frame.setSize(800, 600);
+        frame.setLocationRelativeTo(null);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+
 
     }
 }
